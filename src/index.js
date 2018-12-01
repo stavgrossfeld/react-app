c9d675ba67078676f7cc5230231314c56575787d
Stav Grossfeld <stav.grossfeld@gmail.com>
2018-12-01 12:45:53 +0200

    add components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from './pages/index';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+import App from "./pages/app"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+var destination = document.querySelector("#container");
+  
+ReactDOM.render(
+    <div>
+        <p>Hello stav!</p>
+        <App/>
+    </div>,
+    destination
+);
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import TodoItems from "./ToDoItems";
 
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
+import "../ToDoList.css"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: []};
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+
+  }
+  addItem(e){
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now(),
+        type: typeof(this._inputElement.value)
+      };
+   
+      this.setState((prevState) => {
+        return { 
+          items: prevState.items.concat(newItem) 
+        };
+      });
+     
+      this._inputElement.value = "";
+    }
+    
+     
+    console.log(this.state.items);
+       
+    e.preventDefault();
+  }
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return (item.key !== key);
+    });
+   
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+        
+          <form onSubmit={this.addItem}>
+          
+            <input ref={(a) => this._inputElement = a}
+            placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+          <TodoItems entries={this.state.items}
+                     delete={this.deleteItem}/>
+
+      </div>
+    );
+  }
 }
-
-ReactDOM.render(<App />, document.querySelector('#app'));
+ 
+export default App;
